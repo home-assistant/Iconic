3df3e132ed79999852c174dd0a00e7ff6bde5395
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-06-25 22:04:04 -0700

    Drafts edge insets support, for horizontal and vertical margin for icons

--- Samples/Playground.playground/Contents.swift
+++ Samples/Playground.playground/Contents.swift
@@ -4,15 +4,39 @@ XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 import Iconic
 
+print(IconicVersionNumber)
 
-var str = "Hello, playground"
-
+// Registering the icon font
 Iconic.registerFontAwesomeIcon()
 
+
+// Icon font object
+let font:UIFont = Iconic.fontAwesomeIconFontOfSize(23)!
+
+// Icon as unicode
 let unicode = Iconic.unicodeStringForFontAwesomeIcon(.Bolt)
+let edgeInsets = UIEdgeInsetsMake(0, 5, 0, 20)
 
-let font = Iconic.fontAwesomeIconFontOfSize(20)
+let iconAttributes = [NSFontAttributeName: font,
+                      NSForegroundColorAttributeName: UIColor.orangeColor(),
+                      NSBaselineOffsetAttributeName: edgeInsets.bottom-edgeInsets.top]
 
-let string = Iconic.attributedStringForFontAwesomeIcon(.Bolt, size: 20, color: .whiteColor())
+let iconString = " " + unicode! + " "
 
-let image = Iconic.imageForFontAwesomeIcon(.Bolt, size: 20, color: .whiteColor())
+// Icon as text
+let iconAttrString = NSAttributedString(string: iconString, attributes: iconAttributes).mutableCopy()
+
+print(iconAttrString.length as Int)
+
+// Sets left and right margins
+iconAttrString.setAttributes([NSKernAttributeName: edgeInsets.left], range: NSMakeRange(0, 1))
+iconAttrString.setAttributes([NSKernAttributeName: edgeInsets.right], range: NSMakeRange(2, 1))
+
+let titleAttributes = [NSFontAttributeName: UIFont.systemFontOfSize(25),
+                       NSForegroundColorAttributeName: UIColor.redColor()]
+
+let titleAttrString = NSAttributedString(string: NSStringFromFontAwesomeIcon(.Bolt), attributes: titleAttributes)
+
+let attributedText = NSMutableAttributedString()
+attributedText.appendAttributedString(iconAttrString as! NSAttributedString)
+attributedText.appendAttributedString(titleAttrString)
