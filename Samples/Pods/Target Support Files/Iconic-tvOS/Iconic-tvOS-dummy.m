c3fce5b6b487867997ff11309f16de6d8178531b
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-08-23 18:25:29 -0300

    Cleans pod installation in sample project

--- Samples/Pods/Target Support Files/Iconic-tvOS/Iconic-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Iconic_tvOS : NSObject
-@end
-@implementation PodsDummy_Iconic_tvOS
-@end
--- Samples/Pods/Target Support Files/Iconic-tvOS/Iconic-tvOS-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double IconicVersionNumber;
-FOUNDATION_EXPORT const unsigned char IconicVersionString[];
-
