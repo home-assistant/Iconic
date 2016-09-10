d7353cab90a0378e4eda45243bc76e8135ecbcbc
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-09-10 02:32:18 -0300

    Adds more image conversion snapshot tests

--- Samples/SnapshotTests/ImageIconTests.swift
+++ Samples/SnapshotTests/ImageIconTests.swift
@@ -17,16 +17,40 @@ class ImageIconTests: FBSnapshotTestCase {
         self.recordMode = false
     }
     
-    func testIconSizes() {
+    func testImageDefaultColor() {
         
-        let sizes:[CGFloat] = [16.0, 32.0, 64.0, 128.0, 512.0]
+        let image = Iconic.image(forIcon: Icon.Save, size: 60, color: nil)
+        let imageView = UIImageView(image: image)
+        
+        self.verifyView(imageView, withIdentifier: "")
+    }
+    
+    func testImagePatternColor() {
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let pattern = UIImage(named: "pattern", inBundle: bundle, compatibleWithTraitCollection: nil)
+        let color = UIColor(patternImage: pattern!)
+        
+        let image = Iconic.image(forIcon: Icon.WarningSign, size: 60, color: color)
+        let imageView = UIImageView(image: image)
+        
+        self.verifyView(imageView, withIdentifier: "")
+    }
+    
+    func testImageSizes() {
+        
+        let sizes = [16, 32, 64, 128, 512]
         
         for size in sizes {
             
-            let image = Iconic.image(forIcon: Icon.GithubAlt, size: size, color: UIColor.redColor())
+            let image = Iconic.image(forIcon: Icon.GithubAlt, size: CGFloat(size), color: UIColor.redColor())
             let imageView = UIImageView(image: image)
             
-            self.verifyView(imageView, withIdentifier: "\(size) icon")
+            self.verifyView(imageView, withIdentifier: "\(size)_icon")
         }
+    }
+    
+    func testImageEdgeInsets() {
+        // TODO: Make image(idx:size:color:edgeInsets:) public
     }
 }
