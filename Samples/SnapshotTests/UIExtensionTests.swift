40ec5b934a81cee57481880249236791a4b59f3f
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-09-10 03:14:02 -0300

    Adds attributed string conversion snapshot tests

---
+++ Samples/SnapshotTests/UIExtensionTests.swift
@@ -0,0 +1,14 @@
+//
+//  UIExtensionTests.swift
+//  Samples
+//
+//  Created by Ignacio Romero on 9/10/16.
+//  Copyright © 2016 DZN. All rights reserved.
+//
+
+import FBSnapshotTestCase
+import Iconic
+
+class UIExtensionTests: FBSnapshotTestCase {
+
+}
