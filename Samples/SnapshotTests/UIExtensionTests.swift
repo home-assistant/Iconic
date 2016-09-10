2bc7a9dfed2ddcdc48a5c303078794c3212d5d23
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-09-10 03:43:24 -0300

    Adds UIKit extension snapshot tests

--- Samples/SnapshotTests/UIExtensionTests.swift
+++ Samples/SnapshotTests/UIExtensionTests.swift
@@ -10,5 +10,43 @@ import FBSnapshotTestCase
 import Iconic
 
 class UIExtensionTests: FBSnapshotTestCase {
-
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.recordMode = false
+    }
+    
+    func testUIBarButtonItem() {
+        
+        let buttonItem = UIBarButtonItem(icon: .Spinner, size: 40, target: nil, action: nil)
+        
+        let navItem = UINavigationItem()
+        navItem.leftBarButtonItem = buttonItem
+        
+        let navbar = UINavigationBar(frame: CGRectMake(0, 0, 44, 44))
+        
+        navbar.setItems([navItem], animated: false)
+        
+        self.verifyView(navbar, withIdentifier: "")
+    }
+    
+    func testUITabBarItem() {
+        
+        let item = UITabBarItem(icon: .Sun, size: 40, title: "tab", tag: 0)
+        let tabbar = UITabBar(frame: CGRectMake(0, 0, 80, 80))
+        
+        tabbar.items = [item]
+        
+        self.verifyView(tabbar, withIdentifier: "")
+    }
+    
+    func testUIButton() {
+        
+        let button = UIButton(type: .System)
+        button.setIcon(.Download, size: 40, forState: .Normal)
+        button.sizeToFit()
+        
+        self.verifyView(button, withIdentifier: "")
+    }
 }
