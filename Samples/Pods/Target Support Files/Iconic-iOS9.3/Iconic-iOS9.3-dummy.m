15cacc3fe66acaa15c9be98bd2a7f97b79aac05e
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-11-01 01:43:12 -0700

    Deletes pod files from Sample and ignores them from version control

--- Samples/Pods/Target Support Files/Iconic-iOS9.3/Iconic-iOS9.3-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Iconic_iOS9_3 : NSObject
-@end
-@implementation PodsDummy_Iconic_iOS9_3
-@end
--- Samples/Pods/Target Support Files/Iconic-tvOS9.0/Iconic-tvOS9.0-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Iconic_tvOS9_0 : NSObject
-@end
-@implementation PodsDummy_Iconic_tvOS9_0
-@end
