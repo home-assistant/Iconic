b4a1a1804dee8084f66d112053db152bf55170ab
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-12-01 01:02:19 -0800

    Tweaks the Iconic header view's style/appearance

--- Samples/Playground.playground/Pages/Iconic header design.xcplaygroundpage/Contents.swift
+++ Samples/Playground.playground/Pages/Iconic header design.xcplaygroundpage/Contents.swift
@@ -1,9 +1,78 @@
 
-import Iconic
 import PlaygroundSupport
+import Foundation
+import UIKit
+import Iconic
 
-// First of all, we must registering the icon font
+
+// REGISTRATION
+// First, register Font Awesome icons with Iconic
 FontAwesomeIcon.register()
+
+// Register OpenSans fonts
+let fontNames = ["OpenSans-Bold", "OpenSans-Regular", "OpenSans-Semibold"].flatMap {
+    let url = Bundle.main.url(forResource: $0, withExtension: "ttf") as CFURL!
+    CTFontManagerRegisterFontsForURL(url!, .none, nil)
+}
+
+
+// MARK: - LAYOUT AND DRAWING
+// Draws a background filled with small icons and the title/subtilte of Iconic
+class IconicHeaderView : UIView {
+    
+    var collectionView: IconCollectionView
+    var titleLabel: UILabel
+    
+    init() {
+        let frame = CGRect(x: 0, y: 0, width: 1777, height: 350)
+        let layout = IconCollectionViewLayout(columns: 32, width: frame.width)
+        
+        let viewFrame = CGRect(x: -50, y: -frame.height/2 - 50, width: frame.width + 100, height: (frame.height * 2) + 100)
+        collectionView = IconCollectionView(frame: viewFrame, collectionViewLayout: layout)
+        collectionView.iconColor = UIColor(red: 240/255, green: 109/255, blue: 84/255, alpha: 1)
+        collectionView.backgroundColor = UIColor(red: 215/255, green: 97/255, blue: 76/255, alpha: 1)
+        
+        titleLabel = UILabel(frame: frame)
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 0
+        
+        super.init(frame: frame)
+        
+        configureLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureLayout() {
+        
+        let title = "Iconic"
+        let subtitle = "Render vector icons, natively."
+        
+        let titleAttributes = [NSFontAttributeName: UIFont(name: "OpenSans-Bold", size: 150),
+                               NSForegroundColorAttributeName: UIColor.white]
+        
+        
+        let subtitleAttributes = [NSFontAttributeName: UIFont(name: "OpenSans-Semibold", size: 50),
+                                  NSForegroundColorAttributeName: UIColor.white]
+        
+        let titleString = NSAttributedString(string: title + "\n", attributes: titleAttributes)
+        let subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
+        
+        let attributedText = NSMutableAttributedString()
+        attributedText.append(titleString)
+        attributedText.append(subtitleString)
+        
+        titleLabel.attributedText = attributedText
+        
+        addSubview(collectionView)
+        addSubview(titleLabel)
+        
+        let angle = CGFloat(-15*(M_PI/180))
+        collectionView.transform = CGAffineTransform(rotationAngle: angle)
+    }
+}
 
 class IconCollectionViewLayout : UICollectionViewFlowLayout {
     
@@ -12,7 +81,7 @@ class IconCollectionViewLayout : UICollectionViewFlowLayout {
         
         let spacing: CGFloat = 20
         let padding: CGFloat = spacing/2
-        let insets = UIEdgeInsetsMake(padding, padding, padding, padding)
+        let insets = UIEdgeInsetsMake(-spacing, -spacing, -spacing, -spacing)
         
         let viewGaps = (CGFloat(columns-1) * spacing) + insets.left + insets.right
         let itemWidth = (width - viewGaps) / CGFloat(columns)
@@ -52,14 +121,20 @@ class IconCollectionViewCell : UICollectionViewCell {
 
 class IconCollectionView : UICollectionView, UICollectionViewDataSource {
     
-    let kIconColor = UIColor(red: 63/255, green: 139/255, blue: 199/255, alpha: 1)
-    let kBackgroundColor = UIColor(red: 57/255, green: 125/255, blue: 178/255, alpha: 1)
+    var iconColor: UIColor? {
+        didSet {
+            self.reloadData() // reloads all icons
+        }
+    }
+    
     let kIdentifier = "identifier"
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
         
-        self.backgroundColor = kBackgroundColor
+        iconColor = UIColor.gray
+        backgroundColor = UIColor.darkGray
+        
         self.dataSource = self
         self.register(IconCollectionViewCell.self, forCellWithReuseIdentifier: kIdentifier)
     }
@@ -73,62 +148,23 @@ class IconCollectionView : UICollectionView, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kIdentifier, for: indexPath) as! IconCollectionViewCell
         
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kIdentifier, for: indexPath) as! IconCollectionViewCell
         let layout = collectionView.collectionViewLayout as! IconCollectionViewLayout
         
         if let icon = FontAwesomeIcon(rawValue: indexPath.row) {
-           cell.imageView.image = icon.image(ofSize: layout.estimatedItemSize, color: kIconColor)
+            cell.imageView.image = icon.image(ofSize: layout.estimatedItemSize, color: iconColor)
         }
         
         return cell
     }
 }
 
-class IconicHeaderView : UIView {
-    
-    init() {
-        let frame = CGRect(x: 0, y: 0, width: 1777, height: 350)
-        super.init(frame: frame)
-        
-        let layout = IconCollectionViewLayout(columns: 40, width: frame.width)
-        let collectionView = IconCollectionView(frame: frame, collectionViewLayout: layout)
-        addSubview(collectionView)
 
-        let label = UILabel(frame: frame)
-        label.textColor = UIColor.white
-        label.font = UIFont.systemFont(ofSize: 150)
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        
-        let title = "Iconic\n"
-        let subtitle = "Render vector icons, natively."
-        
-        let titleAttributes = [NSFontAttributeName: UIFont.init(name: "Menlo", size: 150),
-                               NSForegroundColorAttributeName: UIColor.white]
-        
-        let subtitleAttributes = [NSFontAttributeName: UIFont.init(name: "Menlo", size: 60),
-                                  NSForegroundColorAttributeName: UIColor.white]
-        
-        var titleString = NSAttributedString(string: title, attributes: titleAttributes)
-        var subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
-        
-        let attributedText = NSMutableAttributedString()
-        attributedText.append(titleString)
-        attributedText.append(subtitleString)
-        
-        label.attributedText = attributedText
-        addSubview(label)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+// MARK: - Display
 
 let view = IconicHeaderView()
 
-// Shows the final render in the Assisstant Editor
-PlaygroundPage.current.liveView = view
-
+// Shows the render in the Assisstant Editor
+PlaygroundPage.current.liveView = IconicHeaderView()
 
