a5630443b58f921cb47c2fd0e2b4fcb9ceb20341
Joel Fischer <joeljfischer@gmail.com>
2018-03-26 19:41:50 -0400

    Update for Swift 4
    * Update .swiftlint.yml to allow enum names

--- Source/IconDrawable.swift
+++ Source/IconDrawable.swift
@@ -94,10 +94,10 @@ extension IconDrawable {
     public func attributedString(ofSize pointSize: CGFloat, color: UIColor?) -> NSAttributedString {
         
         let font = Self.font(ofSize: pointSize)
-        var attributes = [NSFontAttributeName : font] as [String : AnyObject]
+        var attributes = [NSAttributedStringKey.font : font] as [NSAttributedStringKey : Any]
         
         if let color = color {
-            attributes[NSForegroundColorAttributeName] = color
+            attributes[NSAttributedStringKey.foregroundColor] = color
         }
         
         return NSAttributedString(string: unicode, attributes: attributes)
@@ -110,10 +110,10 @@ extension IconDrawable {
         
         let range = NSRange(location: 0, length: mString.length)
 
-        mString.addAttribute(NSBaselineOffsetAttributeName, value: edgeInsets.bottom-edgeInsets.top, range: range)
+        mString.addAttribute(NSAttributedStringKey.baselineOffset, value: edgeInsets.bottom-edgeInsets.top, range: range)
         
-        let leftSpace = NSAttributedString(string: " ", attributes: [NSKernAttributeName: edgeInsets.left])
-        let rightSpace = NSAttributedString(string: " ", attributes: [NSKernAttributeName: edgeInsets.right])
+        let leftSpace = NSAttributedString(string: " ", attributes: [NSAttributedStringKey.kern: edgeInsets.left])
+        let rightSpace = NSAttributedString(string: " ", attributes: [NSAttributedStringKey.kern: edgeInsets.right])
         
         mString.insert(rightSpace, at: mString.length)
         mString.insert(leftSpace, at: 0)
@@ -142,7 +142,7 @@ extension IconDrawable {
         
         let range = NSRange(location: 0, length: mString.length)
         
-        mString.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: range)
+        mString.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: range)
         
         // Renders the attributed string as image using Text Kit
         UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
