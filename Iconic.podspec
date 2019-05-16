662ba7521ca288e9619aebf2addddaaeabd31261
Robbie Trencheny <me@robbiet.us>
2019-05-15 19:30:45 -0700

    Minor odds and ends

--- Iconic.podspec
+++ Iconic.podspec
@@ -1,4 +1,4 @@
-@version = '1.5.1'
+@version = '1.4'
 
 Pod::Spec.new do |s|
 
--- Source/IconImageView.swift
+++ Source/IconImageView.swift
@@ -9,53 +9,51 @@
 import UIKit
 
 #if os(iOS) || os(tvOS)
-    
-    /** An Image View subclass, capable of rendering icons. Only supported for iOS and tvOS. */
-    public class IconImageView: UIImageView {
-        
-        // MARK: - Public Variables
-        
-        /** The icon drawable to be used as image. */
-        public var iconDrawable: IconDrawable? {
-            didSet {
-                updateIconImage()
-            }
-        }
-        
-        // MARK: - Overrides
-        
-        public override var frame: CGRect {
-            didSet {
-                updateIconImage()
-            }
-        }
-        
-        public override func layoutSubviews() {
-            super.layoutSubviews()
-            
+/** An Image View subclass, capable of rendering icons. Only supported for iOS and tvOS. */
+public class IconImageView: UIImageView {
+
+    // MARK: - Public Variables
+
+    /** The icon drawable to be used as image. */
+    public var iconDrawable: IconDrawable? {
+        didSet {
             updateIconImage()
         }
-        
-        // MARK: - Image Constructor
-        
-        /**
-         Updates the icon image, only when the frame is not empty.
-         */
-        fileprivate func updateIconImage() {
-            
-            // No need to update the icon with empty frame
-            if frame.isEmpty {
-                return
-            }
-            
-            if let icon = iconDrawable {
-                let image = icon.image(ofSize: frame.size, color: nil)
-                self.image = image.withRenderingMode(.alwaysTemplate)
-            } else {
-                self.image = nil
-            }
+    }
+
+    // MARK: - Overrides
+
+    public override var frame: CGRect {
+        didSet {
+            updateIconImage()
         }
     }
-    
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        updateIconImage()
+    }
+
+    // MARK: - Image Constructor
+
+    /**
+     Updates the icon image, only when the frame is not empty.
+     */
+    fileprivate func updateIconImage() {
+
+        // No need to update the icon with empty frame
+        if frame.isEmpty {
+            return
+        }
+
+        if let icon = iconDrawable {
+            let image = icon.image(ofSize: frame.size, color: nil)
+            self.image = image.withRenderingMode(.alwaysTemplate)
+        } else {
+            self.image = nil
+        }
+    }
+}
 #endif
 
